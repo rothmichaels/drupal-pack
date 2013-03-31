ccd076223b7d8dff080a8984af6be6d8469e88b0
Roth Michaels <roth@rothmichaels.us>
2013-03-31 17:32:57 -0400

    Using web-mode instead of drupal-mode and php-mode.

--- init.el
+++ init.el
@@ -19,7 +19,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(live-add-pack-lib "php-mode")
-(live-add-pack-lib "drupal-mode")
-(require 'drupal-mode) ; drupal-modes requires php-mode
+;(live-add-pack-lib "php-mode")
+;(live-add-pack-lib "drupal-mode")
+;(require 'drupal-mode)
+                                        ; drupal-modes requires php-mode
 
+(live-add-pack-lib "web-mode")
+(require 'web-mode)
