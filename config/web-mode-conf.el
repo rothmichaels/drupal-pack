8fd2d2cdc62812cc39735ed296342b68e8f0ca8c
Roth Michaels <roth@rothmichaels.us>
2013-04-11 18:04:05 -0400

    web-mode setup

--- config/web-mode-conf.el
+++ config/web-mode-conf.el
@@ -1,4 +1,4 @@
-;; init.el
+;; web-mode-conf.el
 ;; drupal-pack
 ;; A pack for Emacs Live for Drupal development
 
@@ -20,11 +20,13 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-(live-add-pack-lib "web-mode")
-(require 'web-mode)
-(live-load-config-file "web-mode-conf.el")
-
-(live-add-pack-lib "php-mode")
-(require 'php-mode)
-(live-add-pack-lib "drupal-mode")
-(require 'drupal-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
